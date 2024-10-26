03789808df32ee099229f513a8c4cbc12a399a5e
jasmineamber <jasmineamber@aliyun.com>
2024-10-26 17:06:21 +0800

    修改标题，语言

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: '别说废话',
-  subtitle: '',
+  title: '德克斯特的实验室',
+  subtitle: '别说废话',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
